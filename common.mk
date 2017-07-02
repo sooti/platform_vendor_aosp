79f00bf4ee5cd484b57da364ab81ec9cf598a0de
sooti <sooti85@gmail.com>
2017-07-02 12:13:29 +0000

    aosp: port some stuff from g3-aosp

--- common.mk
+++ common.mk
@@ -46,10 +46,6 @@ PRODUCT_COPY_FILES +=  \
     vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
-# Enable SIP+VoIP on all targets
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
-
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
@@ -160,3 +156,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 #endif
 
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+
+# Glove mode
+PRODUCT_PACKAGES += \
+    DeviceSettings
